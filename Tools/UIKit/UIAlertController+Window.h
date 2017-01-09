29c3e853ce03451d60d57c20bc652cdb0e8dab72
Lincon Zhang <zhangyilin@letv.com>
2017-01-09 23:35:44 +0800

    增加对UIAlertController的扩展

---
+++ Tools/UIKit/UIAlertController+Window.h
@@ -0,0 +1,16 @@
+//
+//  UIAlertController+Window.h
+//  DeveloperTools
+//
+//  Created by zhang on 2017/1/9.
+//  Copyright © 2017年 YLDeveloperTools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (Window)
+
+- (void)show;
+- (void)show:(BOOL)animated;
+
+@end
